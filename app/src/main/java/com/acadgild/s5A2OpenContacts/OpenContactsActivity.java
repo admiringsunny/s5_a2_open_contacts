e509c3e871f6617682aff610e14d6a9af52dba42
admiringsunny <admiringsunny@gmail.com>
2016-08-25 14:30:24 +0530

    Initial commit

---
+++ app/src/main/java/com/acadgild/s5A2OpenContacts/OpenContactsActivity.java
@@ -0,0 +1,21 @@
+package com.acadgild.s5A2OpenContacts;
+
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class OpenContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_contacts);
+    }
+
+    public void openContacts(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+}
